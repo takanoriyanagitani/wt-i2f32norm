bb6158d44ab2d4719d6e5c2473c7be1dcc2ce5ff
yanagitani <takanori.yanagitani@gmail.com>
2026-06-12 22:34:19 +0000

    chore: wasi related shells

---
+++ opt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+iwasi=./i2f32norm.wasm
+
+wasm-opt \
+	-Oz \
+	-o opt.wasm \
+	--enable-simd \
+	--enable-relaxed-simd \
+	--enable-bulk-memory \
+	--enable-nontrapping-float-to-int \
+	--enable-multivalue \
+	--enable-tail-call \
+	"${iwasi}"
---
+++ wasm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+iname=./i2f32norm.wat
+oname=./i2f32norm.wasm
+
+wat2wasm \
+	"${iname}" \
+	-o "${oname}" \
+	--enable-relaxed-simd \
+	--enable-tail-call || exec sh -c '
+		echo unable to compile.
+		exit 1
+	'
+
+ls -l "${oname}"
